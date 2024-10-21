3be8264c704b30236f42e2430d80ba0702d05ebf
Epsilon <141088741+Epsilon017@users.noreply.github.com>
2024-10-20 23:55:01 -0400

    Add index.html and canvas element with full-screen dimensions

---
+++ script.ts
@@ -0,0 +1,4 @@
+// setup main display canvas that covers the whole body
+const canvas = document.getElementById('onscreenCanvas') as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
