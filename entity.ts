a6d75569897ef8bf300e3c538236467a3e7d015e
Epsilon <141088741+Epsilon017@users.noreply.github.com>
2024-10-22 11:06:24 -0400

    Add component system to modularly extend entity functionality

--- entity.ts
+++ entity.ts
@@ -1,12 +1,52 @@
+import { Component } from "./component.js";
+
 export class Entity {
 
 
+    private components : Array<Component> = [];
+
+
+    addComponent<T extends Component>(componentClass : new () => T) : T {
+
+        // return existing component if one was already added
+        let existingComponent = this.getComponent(componentClass);
+        if (existingComponent !== null) { return existingComponent; };
+
+        let newComponent = new componentClass();
+        this.components.push(newComponent);
+        return newComponent as T;
+
+    };
+
+
+    getComponent<T extends Component>(componentClass : new () => T) : T | null {
+
+        for (let component of this.components) {
+            if (component instanceof componentClass) {
+                return component as T;
+            };
+        };
+
+        // return null if no component of that class was found
+        return null;
+
+    };
+
+
     update() {
+
+        for (let component of this.components) {
+            component.update();
+        };
 
     };
 
 
     draw() {
+
+        for (let component of this.components) {
+            component.draw();
+        };
 
     };
 
