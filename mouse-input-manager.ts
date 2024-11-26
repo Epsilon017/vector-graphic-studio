8317a94ab0c08c27a57099f90561db2079366677
Epsilon <141088741+Epsilon017@users.noreply.github.com>
2024-11-25 21:07:00 -0500

    Update MouseInputManager to call a Mouse component's click function upon being clicked

--- mouse-input-manager.ts
+++ mouse-input-manager.ts
@@ -1,3 +1,10 @@
+import { isPointInBox } from "./collision.js";
+import { C_BoxCollider } from "./components/box-collider.js";
+import { C_Mouse } from "./components/mouse.js";
+import { Entity } from "./scene system/entity.js";
+import { SceneManager } from "./scene system/scene-manager.js";
+import { Vector2 } from "./vector2.js";
+
 export class MouseInputManager { // singleton for handling and broadcasting the user's mouse inputs
     
 
@@ -22,7 +29,47 @@ export class MouseInputManager { // singleton for handling and broadcasting the 
     };
 
 
-    handleMouseDown(event : MouseEvent) {};
+    handleMouseDown(event : MouseEvent) {
+
+        let mouseDownPosition = new Vector2(event.clientX, event.clientY);
+        let mouseEntities = this.getMouseEntities();
+
+        let clickedEntity = null;
+        for (let ent of mouseEntities) {
+
+            let entBoxCollider = ent.getComponent(C_BoxCollider);
+            if (entBoxCollider !== null && isPointInBox(mouseDownPosition, entBoxCollider)) {
+                clickedEntity = ent;
+                break;
+            };
+
+        };
+
+        if (clickedEntity !== null) {
+
+            let mouseComp = clickedEntity.getComponent(C_Mouse);
+            mouseComp.leftClick();
+
+        };
+
+    };
+
+    private getMouseEntities() {
+
+        let mouseEntities : Array<Entity> = [];
+        let entities = SceneManager.getInstance().currentScene.entityCollection.getEntities();
+
+        for (let ent of entities) {
+
+            if (ent.getComponent(C_Mouse) !== null) {
+                mouseEntities.push(ent);
+            };
+
+        };
+
+        return mouseEntities;
+
+    };
 
 
 };
